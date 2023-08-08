90776294b8e53275c71afcbb5d239e0f663015ed
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-08-08 18:21:05 +0530

    Create DrawCards.cs

---
+++ Card Game/Assets/Scripts/DrawCards.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card1, Card2;
+    public GameObject PlayerArea, OpponentArea;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
